dff928a69a5d5d6b1ea82d00582f49e78ce45616
Riza Noor Fauzan Prayudi <riza.prayudi@jatis.com>
2022-10-13 06:45:04 +0700

    learning case condition

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
+
 import "fmt"
 
 func main()  {
-	/** 
-	age := 20
+	number := 2
 
-		if age > 10 {
-			fmt.Println("boleh main game")
-		}else {
-			fmt.Println("tidak boleh main game")
-		}
-	*/
-
-	score := 65
-	var grade string
-	
-		if score <= 50 {
-			grade = "E"
-		} else if score <= 60{
-			grade = "D"
-		} else if score < 70 {
-			grade = "C"
-		}else {
-			grade = "NULL"
-		}
-
-		fmt.Println(grade)
-
-
-
+	switch number {
+	case 1:
+		fmt.Println("Satu")
+	case 2:
+		fmt.Println("Dua")
+	case 3:
+		fmt.Println("Tiga")
+	default:
+		fmt.Println("Default")
+	}
 }
