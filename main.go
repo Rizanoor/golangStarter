87a1245bfc701f1002b4a055f466804a119d12a8
Riza Noor Fauzan Prayudi <riza.prayudi@jatis.com>
2022-10-13 05:42:02 +0700

    learning package

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hello/calculation"	
+)
 
 func main()  {
 	fmt.Println("hallo world")
+
+	result := calculation.Add(9, 9)
+
+	fmt.Println(result)
+
 }
